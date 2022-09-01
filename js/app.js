14ae876e87551873d4871733300d53bfb7c4b74d
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2022-09-01 18:56:05 +0200

    update du numéro de build

--- js/app.js
+++ js/app.js
@@ -313,7 +313,7 @@ const ptr = PullToRefresh.init({
 // show edt first 
 var latestVersion = localStorage.getItem('latestVersion')
 
-const version = "3.4.1 beta";
+const version = "3.4.2 beta";
 const release = '3.4';
 
 function openApp() {
